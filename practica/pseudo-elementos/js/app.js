bc38f4cdfbb7e18d87c19d4693b5324350433352
Mr.HOLK <mrh@localhost.localdomain>
2023-01-09 07:25:54 -0400

    Proyecto version 0.1

---
+++ practica/pseudo-elementos/js/app.js
@@ -0,0 +1,34 @@
+const menu = document.querySelector(".menu");
+const openMenuBtn = document.querySelector(".open-menu");
+const closeMenuBtn = document.querySelector(".close-menu");
+
+function toggleMenu() {
+    menu.classList.toggle("menu-opened");
+}
+
+openMenuBtn.addEventListener("click", toggleMenu)
+closeMenuBtn.addEventListener("click", toggleMenu)
+
+
+let navegador = navigator.userAgent;
+if (navigator.userAgent.match(/Android/i) || navigator.userAgent.match(/webOS/i) || navigator.userAgent.match(/iPhone/i) || navigator.userAgent.match(/iPad/i) || navigator.userAgent.match(/iPod/i) || navigator.userAgent.match(/BlackBerry/i) || navigator.userAgent.match(/Windows Phone/i) || window.innerWidth <= 800) {
+
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 2,
+    spaceBetween: 10,
+    slidesPerGroup: 2,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+} else {
+    console.log("No estás usando un móvil");
+}
