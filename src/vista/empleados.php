762df092176edbdbde4868ad8fb573628d4943ab
Sr-Hacker <leojmelendezd@gmail.com>
2024-07-08 13:09:19 -0400

    change background and add search-bar styles

--- src/vista/empleados.php
+++ src/vista/empleados.php
@@ -1,16 +1,16 @@
-<html>
+<!DOCTYPE html>
   <?php require_once("common/header.php"); ?>
   <link rel="stylesheet" href="css/modal.css">
   <link rel="stylesheet" href="css/empleado.css">
-
-<body>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,0,0" />
+  <body>
   <div>
     <h2>Registro De Empleados</h2>
     <div class="container_empleados">
       <div class="container_empleados--menu">
         <div class="container_empleados--search">
-          <input placeholder="cedula" type="number">
-          <button class="button">buscar</button>
+          <input class="buscador" placeholder="cedula" type="number">
+          <button class="button"><span class="material-symbols-rounded">search</span></button>
         </div>
         <button id="include">Crear Nuevo Empleado</button>
       </div>
