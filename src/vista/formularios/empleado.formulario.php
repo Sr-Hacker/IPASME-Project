aa642a3764e49bbb60920b61fba5f2f1f303f2d6
Sr-Hacker <leojmelendezd@gmail.com>
2024-11-05 22:49:59 -0400

    odificando modal

--- src/vista/formularios/empleado.formulario.php
+++ src/vista/formularios/empleado.formulario.php
@@ -1,10 +1,10 @@
 <div class="container_empleados">
   <h5>Crear Nuevo Empleado</h5>
   <div class="container_empleados--form">
-    <input type="text" placeholder="Cedula" id="ced_empleado" name="ced_empleado">
+    <input type="number" placeholder="Cedula" id="ced_empleado" name="ced_empleado">
     <input type="text" placeholder="Nombre" id="nombre" name="nombre">
     <input type="text" placeholder="Apellido" id="apellido" name="apellido">
-    <input type="text" placeholder="Telefono" id="telefono" name="telefono">
+    <input type="number" placeholder="Telefono" id="telefono" name="telefono">
     <input type="date" placeholder="fecha de nacimiento" id="fecha_nacimiento" name="fecha_nacimiento">
     <input type="password" placeholder="Contrasena" id="contrasena" name="contrasena">
     <input type="text" placeholder="estado" id="estado_provincia" name="estado_provincia">
@@ -12,8 +12,8 @@
     <input type="text" placeholder="direccion" id="direccion" name="direccion">
     <input type="text" placeholder="codigo_postal" id="codigo_postal" name="codigo_postal">
     <input type="text" placeholder="numero_casa" id="numero_casa" name="numero_casa">
-    <input type="email" placeholder="correo" id="correo" name="correo">
-    <select id="rol" name="rol">
+    <input type="mail" placeholder="correo" id="correo" name="correo">
+    <select class="opc-modal" id="rol" name="rol">
       <option value="admin">administrador</option>
       <option value="user">empleado</option>
     </select>
