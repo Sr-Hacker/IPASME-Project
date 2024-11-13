469f9ca00c6914e844596d6e2e10ba6d6d2c6612
jmg24a <jmg24a@gmail.com>
2024-11-13 17:06:41 -0400

    commit

--- src/vista/formularios/medico.formulario.php
+++ src/vista/formularios/medico.formulario.php
@@ -10,6 +10,29 @@
       <option value="1">Externo</option>
     </select>
 
+    <select name="dias_semana" id="dias_semana" multiple="true">
+      <option value="Turno matutino">manana</option>
+      <option value="Turno vespertino">tarde</option>
+      <option value="Turno Completo">Miércoles</option>
+    </select>
+
+    <label><input type="checkbox" name="opciones" value="opcion1"> Opción 1</label><br>
+    <label><input type="checkbox" name="opciones" value="opcion2"> Opción 2</label><br>
+    <label><input type="checkbox" name="opciones" value="opcion3"> Opción 3</label><br>
+    <label><input type="checkbox" name="opciones" value="opcion4"> Opción 4</label><br>
+
+
+    <select name="dias_semana" id="dias_semana" multiple="true">
+      <option value="lunes">Lunes</option>
+      <option value="martes">Martes</option>
+      <option value="miercoles">Miércoles</option>
+      <option value="jueves">Jueves</option>
+      <option value="viernes">Viernes</option>
+      <option value="sabado">Sábado</option>
+      <option value="domingo">Domingo</option>
+    </select>
+
+
     <div id="especialidades"></div>
 
     <div>
