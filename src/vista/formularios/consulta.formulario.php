364b8f7590964cd52dca8c55300d7458c48d36a5
jmg24a <jmg24a@gmail.com>
2024-11-15 02:50:59 -0400

    consulta

--- src/vista/formularios/consulta.formulario.php
+++ src/vista/formularios/consulta.formulario.php
@@ -1,12 +1,27 @@
 <div class="container_beneficiarios">
   <h4>Crear Nuevo Beneficiario</h4>
   <div class="container_beneficiarios--form">
+    <label for="">n_consulta</label>
     <input type="number" placeholder="cedula" id="n_consulta" name="n_consulta">
+    <span id="m_n_consulta"></span>
+
+    <label for="">cod_cita</label>
     <input type="number" placeholder="cedula del afiliado" id="cod_cita" name="cod_cita">
+    <span id="m_cod_cita"></span>
+
+    <label for="">n_historia</label>
     <input type="text" placeholder="n_historia" id="n_historia" name="n_historia">
+    <span id="m_n_historia"></span>
+
+    <label for="">motivo</label>
     <input type="text" placeholder="nombre" id="motivo" name="motivo">
+    <span id="m_motivo"></span>
+
+    <label for="">detalle</label>
     <input type="text" placeholder="apellido" id="detalle" name="detalle">
-  <div>
+    <span id="m_detalle"></span>
+
+  </div>
   <button class="btn-modal" id="action_modal">incluir</button>
   <button class="btn-modal" id="closeModal">cancelar</button>
 </div>
