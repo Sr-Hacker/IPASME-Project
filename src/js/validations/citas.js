578bb7f7b37a53ad711166e4408a0c9a1b2d7a18
jmg24a <jmg24a@gmail.com>
2024-11-15 04:48:48 -0400

    citas

--- src/js/validations/citas.js
+++ src/js/validations/citas.js
@@ -1,13 +1,13 @@
 $(document).ready(function(){
 //VALIDACION DE DATOS
-$("#cod_cita").on("keypress",function(e){
-  validarkeypress(/^[0-9-\b]*$/,e);
-});
+  $("#cod_cita").on("keypress",function(e){
+    validarkeypress(/^[0-9-\b]*$/,e);
+  });
 
-$("#cod_cita").on("keyup",function(){
-  validarkeyup(/^[0-9]{7,8}$/,$(this),
-  $("#m_cod_cita"),"El formato debe ser 9999999 ");
-});
+  $("#cod_cita").on("keyup",function(){
+    validarkeyup(/^[0-9]{7,8}$/,$(this),
+    $("#m_cod_cita"),"solo numeros");
+  });
 
 	$("#ced_afiliado").on("keypress",function(e){
 		validarkeypress(/^[0-9-\b]*$/,e);
@@ -15,16 +15,16 @@ $("#cod_cita").on("keyup",function(){
 
 	$("#ced_afiliado").on("keyup",function(){
 		validarkeyup(/^[0-9]{7,8}$/,$(this),
-		$("#m_ced_afiliado"),"El formato debe ser 9999999 ");
+		$("#m_ced_afiliado"),"El formato debe ser 9999999");
 	});
 
 	$("#ced_beneficiario").on("keypress",function(e){
-		validarkeypress(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]*$/,e);
+		validarkeypress(/^[0-9-\b]*$/,e);
 	});
 
 	$("#ced_beneficiario").on("keyup",function(){
-		validarkeyup(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]{3,30}$/,
-		$(this),$("#m_ced_beneficiario"),"Solo letras  entre 3 y 30 caracteres");
+		validarkeyup(/^[0-9]{7,8}$/,
+		$(this),$("#m_ced_beneficiario"),"El formato debe ser 9999999");
 	});
 
 	$("#detalle").on("keypress",function(e){
@@ -39,20 +39,24 @@ $("#cod_cita").on("keyup",function(){
 
 //Validación de todos los campos antes del envio
 function validarEnvio(){
-	if(validarkeyup(/^[0-9]{7,8}$/,$("#cedula"),
-		$("#scedula"),"El formato debe ser 9999999")==0){
-	    muestraMensaje("La cedula debe coincidir con el formato <br/>"+
-						"99999999");
+	if(validarkeyup(/^[0-9]{1,8}$/,$("#cod_cita"),
+		$("#m_cod_cita"),"solo se permiten numeros")==0){
+	    muestraMensaje("solo se permiten numeros");
+		return false;
+	}
+	else if(validarkeyup(/^[0-9]{7,8}$/,
+		$("#ced_afiliado"),$("#m_ced_afiliado"),"formato no valido")==0){
+		muestraMensaje("formato no valido");
+		return false;
+	}
+  else if(validarkeyup(/^[0-9]{7,8}$/,
+		$("#ced_beneficiario"),$("#m_ced_beneficiario"),"formato no valido")==0){
+		muestraMensaje("formato no valido");
 		return false;
 	}
 	else if(validarkeyup(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]{3,30}$/,
-		$("#apellido"),$("#sapellido"),"Solo letras  entre 3 y 30 caracteres")==0){
-		muestraMensaje("Apellidos <br/>Solo letras  entre 3 y 30 caracteres");
-		return false;
-	}
-	else if(validarkeyup(/^[A-Za-z\b\s\u00f1\u00d1\u00E0-\u00FC]{3,30}$/,
-		$("#nombre"),$("#snombre"),"Solo letras  entre 3 y 30 caracteres")==0){
-		muestraMensaje("Nombres <br/>Solo letras  entre 3 y 30 caracteres");
+		$("#detalle"),$("#m_detalle"),"Solo letras  entre 3 y 30 caracteres")==0){
+		muestraMensaje("Solo letras  entre 3 y 30 caracteres");
 		return false;
 	}
 	return true;
--- src/vista/formularios/cita.formulario.php
+++ src/vista/formularios/cita.formulario.php
@@ -6,37 +6,37 @@
       <div class="formulario__grupo">
         <label for="cod_cita" class="formulario__label">Codigo de cita</label>
         <input type="text" class="formulario__input" id="cod_cita" name="cod_cita">
-        <span id="cod_cita"></span>
+        <span id="m_cod_cita"></span>
       </div>
 
       <div class="formulario__grupo">
         <label for="cod_afiliado" class="formulario__label">Cedula afiliado</label>
         <input type="text" id="ced_afiliado" name="ced_afiliado">
-        <span></span>
+        <span id="m_ced_afiliado"></span>
       </div>
 
       <div class="formulario__grupo">
         <label for="ced_beneficiario" class="formulario__label">ced beneficiario</label>
         <input type="text" placeholder="ced_beneficiario" id="ced_beneficiario" name="ced_beneficiario">
-        <span></span>
+        <span id="m_ced_beneficiario"></span>
       </div>
 
       <div class="formulario__grupo">
         <label for="" class="formulario__label">fecha</label>
         <input type="date" id="fecha" name="fecha">
-        <span></span>
+        <span id="m_fecha"></span>
       </div>
 
       <div class="formulario__grupo">
         <label for="" class="formulario__label">hora</label>
         <input type="time" id="hora" name="hora">
-        <span></span>
+        <span id="m_hora"></span>
       </div>
 
       <div class="formulario__grupo">
         <label class="formulario__label" for="">detalle</label>
         <input type="text" placeholder="detalles" id="detalle" name="detalle">
-        <span></span>
+        <span id="m_detalle"></span>
       </div>
 
       <div class="formulario__grupo">
@@ -46,7 +46,7 @@
           <option value="1">Activo</option>
           <option value="0">Cancelado</option>
         </select>
-        <span></span>
+        <span id="m_vigente"></span>
       </div>
 
       <div class="formulario__grupo">
