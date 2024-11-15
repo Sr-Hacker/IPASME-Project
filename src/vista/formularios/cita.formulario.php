48284cf2b9f6e3546de116ca633f8576894428e8
Sr-Hacker <leojmelendezd@gmail.com>
2024-11-15 02:25:43 -0400

    agg estilos a formulario citas.php

--- src/vista/formularios/cita.formulario.php
+++ src/vista/formularios/cita.formulario.php
@@ -1,8 +1,13 @@
 <div class="container_especialidad">
   <h5>Citas</h5>
-    <div class="container_especialidad--form">
-      <div>
-        <input type="text" id="cod_cita" name="cod_cita">
+    <form action="" class="formulario" id="formulario">
+
+    <div class="formulario__grupo" id="grupo__apellidos">
+      <label for="cod_cita" class="formulario__label">Codigo de cita</label>
+      <input type="text" class="formulario__input" id="cod_cita" name="cod_cita">
+      <span id="m_ced_afiliado"></span>
+    </div>
+
         <input type="text" id="ced_afiliado" name="ced_afiliado">
         <input type="date" placeholder="ced_beneficiario" id="ced_beneficiario" name="ced_beneficiario">
         <input type="text" id="cod_especialidad_medico" name="cod_especialidad_medico">
@@ -10,7 +15,7 @@
         <input type="date" placeholder="Fecha" id="hora" name="hora">
         <input type="text" placeholder="detalles" id="detalle" name="detalle">
         <input type="text" placeholder="vigente" id="vigente" name="vigente">
-      </div>
+    </form>
 
       <div>
         <div>
@@ -24,5 +29,4 @@
         <button class="btn-modal" id="action_modal">incluir</button>
         <button class="btn-modal" id="closeModal">cancelar</button>
       </div>
-  </div>
 </div>
